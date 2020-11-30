715c004d2096d7d4988714ba15faa31cffa99272
li song <lisong@114study.com>
2020-11-30 17:14:18 +0800

    11

--- node/07_learn_node/01_Buffer的使用/02_Buffer的alloc的创建方式.js
+++ node/07_learn_node/01_Buffer的使用/02_Buffer的alloc的创建方式.js
@@ -1,5 +1,12 @@
 const buffer  = Buffer.alloc(8);
 
+
 buffer[0] = 88;
 buffer[1] = 0x88;
 console.log(buffer);
+
+/*
+  事实上我们创建Buffer时，
+  并不会频繁的向操作系统申请内存，
+  它会默认先申请一个8*1024的空间
+*/
---
+++ node/07_learn_node/02_事件循环/01_JavaScript代码的执行流程.js
@@ -0,0 +1,16 @@
+
+
+const message = 'hello world';
+
+console.log(message);
+
+function sum (num1, num2) {
+  return num1 + num2
+}
+
+function foo() {
+  const result = sum(20, 30);
+  console.log(result);
+}
+
+foo();
