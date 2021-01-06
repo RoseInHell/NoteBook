f98c3613880cc0793f04ad52a6f6da2655132c11
RoseInHell <242108590@qq.com>
2021-01-06 21:31:45 +0800

    123

---
+++ coder/src/controller/comment.controller.js
@@ -0,0 +1,37 @@
+const commentService = require('../service/comment.service');
+
+
+class CommentController {
+  async create(ctx, next) {
+    // 获取数据
+    const { momentId, content } = ctx.request.body;
+    const { id } = ctx.user; 
+    
+    const result = await commentService.create(momentId, content, id);
+
+    ctx.body = result;
+
+  }
+
+  async reply(ctx, next) {
+    // 获取数据
+    const { momentId, content } = ctx.request.body;
+    const { id } = ctx.user;
+    const { commentId } = ctx.params;
+
+    const result = await commentService.reply(momentId, content, id, commentId);
+
+    ctx.body = result;
+  }
+
+  async update(ctx, next) {
+    const { commentId } = ctx.params;
+    const { content } = ctx.request.body;
+    
+    const result = await commentService.update(commentId, content);
+
+    ctx.body = result;
+  }
+}
+
+module.exports = new CommentController();
--- coder/src/router/comment.router.js
+++ coder/src/router/comment.router.js
@@ -2,24 +2,17 @@ const Router = require('koa-router');
 
 const { verifyAuth, verifyPermission } = require('../middleware/auth.middleware');
 
-const { create, reply, update, remove, list } = require('../controller/comment.controller')
+const { create, reply, update } = require('../controller/comment.controller');
 
 const commentRouter = new Router({prefix: '/comment'});
-
 
 // 评论某个动态
 commentRouter.post('/', verifyAuth, create);
 
-// 对某一条评论进行回复
+// 对某个评论进行回复
 commentRouter.post('/:commentId/reply', verifyAuth, reply);
 
-// 修改评论
+// 修改某个评论
 commentRouter.patch('/:commentId', verifyAuth, verifyPermission, update);
-
-// 删除评论
-commentRouter.delete('/:commentId', verifyAuth, verifyPermission, remove);
-
-// 获取评论列表
-commentRouter.get('/', list);
 
 module.exports = commentRouter;
