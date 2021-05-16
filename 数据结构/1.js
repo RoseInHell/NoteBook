635967f979f971362a00491e99558ddd39ffa63d
RoseInHell <242108590@qq.com>
2021-05-16 17:11:50 +0800

    归并排序上传

--- 数据结构/1.js
+++ 数据结构/1.js
@@ -1,22 +1,3 @@
-var maxProfit = function(prices) {
-  let min = 0;
-  let big = 0;
-  let profit = 0;
-  for(let i=1; i<prices.length; i++) {
-    if (prices[i] < prices[min]) {
-      min = i;
-      big = min + 1 
-    }
-    if (i > min && prices[i] > prices[big]) {
-      big = i
-    }
-    if (prices[big] -prices[min] > profit) {
-      profit = prices[big] -prices[min]
-    }
-  }
-  return profit
-};
+var sumEvenAfterQueries = function(nums, queries) {
 
-console.log(maxProfit([7,1,5,3,6,4]))
-console.log(maxProfit([2,4,1]))
-console.log(maxProfit([1,2]))
+};
