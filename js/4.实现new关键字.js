e6b48597026f617e3cea0787054e91cb94fb02cc
RoseInHell <2421085090@qq.com>
2021-05-31 15:32:48 +0800

    1111

--- js/4.实现new关键字.js
+++ js/4.实现new关键字.js
@@ -7,6 +7,10 @@
 
 function objectFactory() {
   var obj = new Object();
+  /* 
+    取出第一个参数，就是我们要传入的构造函数。
+    此外因为 shift 会修改原数组，所以 arguments 会被去除第一个参数
+  */
   var Constructor = [].shift.call(arguments);
   obj.__proto__ = Constructor.prototype;
   var ret = Constructor.apply(obj, arguments);
@@ -14,3 +18,12 @@ function objectFactory() {
   // ret || obj 这里这么写考虑了构造函数显示返回 null 的情况
   return typeof ret === 'object' ? ret || obj : obj;
 }
+
+function Otaku (name, age) {
+  this.name = name;
+  this.age = age;
+
+  this.habit = 'Games';
+}
+
+var person = objectFactory(Otaku, 'Kevin', '18')
---
+++ js/7.bind实现.js
@@ -0,0 +1,41 @@
+Function.prototype.bind2 = function(context) {
+  if (typeof this !== 'function') {
+    throw new Error("Function.prototype.bind - what is trying to be bound is not callable")
+  }
+
+  var self = this;
+
+  // 获取bind2函数从第二个参数到最后一个参数
+  var args = Array.prototype.slice.call(arguments, 1);
+  // 用空函数做中转
+  var fNOP = function () {};
+  
+  var fbound = function() {
+    var bindArgs = Array.prototype.slice.call(arguments);
+    /* 
+      当作为构造函数时，this 指向实例，self 指向绑定函数，
+      因为下面一句 `fbound.prototype = this.prototype;`，
+      已经修改了 fbound.prototype 为 绑定函数的 prototype，
+      此时结果为 true，当结果为 true 的时候，this 指向实例。
+    */
+    /* 
+      当作为普通函数时，this 指向 window，self 指向绑定函数，
+      此时结果为 false，当结果为 false 的时候，this 指向绑定的 context。
+    */
+    return self.apply(this instanceof self ? this : context, args.concat(bindArgs))
+  }
+  // 修改返回函数的 prototype 为绑定函数的 prototype，实例就可以继承函数的原型中的值
+
+  fNOP.prototype = self.prototype;
+  fbound.prototype = new fNOP();
+  return fbound;
+}
+
+
+function bar() {}
+
+var bindFoo = bar.bind2(null);
+
+bindFoo.prototype.value = 1;
+
+console.log(bar.prototype.value) // 1
