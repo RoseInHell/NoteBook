918c97a09b5f32b91d78724cc7d32fb4b4a2ff5e
RoseInHell <242108590@qq.com>
2021-07-14 22:20:08 +0800

    11

--- 1.js
+++ 1.js
@@ -1,18 +1,25 @@
-var permute = function(nums) {
-  const res = [];
-  for (let i=0; i<nums.length; i++) {
-    for (let j=0; j<nums.length; j++) {
-      var temp = nums[i];
-      nums[i] = nums[j];
-      nums[j] = temp;
-      res.push([...nums])
-      temp = nums[i]
-      nums[i] = nums[j];
-      nums[j] = temp;
-    }
-  }
-  return res;
+function Foo() {
+  getName = function () {
+    console.log(1);
+  };
+  return this;
+}
+Foo.getName = function () {
+  console.log(2);
 };
+Foo.prototype.getName = function () {
+  console.log(3);
+};
+var getName = function () {
+  console.log(4);
+};
+function getName() {
+  console.log(5);
+}
+getName();
+Foo().getName();
+getName();
+Foo.getName();
+new Foo().getName();
+new new Foo().getName();
 
-console.log(permute([1,2,3]))
-// console.log(permute([0,1]))
